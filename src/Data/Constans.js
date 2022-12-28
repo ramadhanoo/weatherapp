4686e8258dd3525ac2336aa8cbb76c40e3815d23
dio dava ramadha <dioramadhan74@gmail.com>
2022-12-28 13:06:42 +0700

    add base url and api key

--- src/Data/Constans.js
+++ src/Data/Constans.js
@@ -1,109 +1,11 @@
 import {Colors, Images} from '../Themes';
-
-export const BASE_URL = 'https://api.sampleapis.com';
-
+export const API_VERSION = '2.5';
+export const BASE_URL = `https://api.openweathermap.org/data/${API_VERSION}`;
 export const API_STATUS = 'dev';
 export const API_STATUS2 = 'staging';
 export const Api_status3 = 'prod';
 
-export const DATA_RUMAH_SAKIT = [
-  {
-    namaRumahSakit: 'rumah sakit coba',
-    daerah: 'Jakarta',
-    lat: -6.0174783,
-    long: 106.049864,
-    jarak: 0,
-  },
-  {
-    namaRumahSakit: 'rumah sakit bintaro',
-    daerah: 'Jakarta',
-    lat: -6.2765517,
-    long: 106.7229619,
-    jarak: 0,
-  },
-];
-
-export const MENU_ACTIONS = [
-  {
-    name: 'Machiato',
-    images: Images.coffe1,
-  },
-  {
-    name: 'Americano',
-    images: Images.coffe2,
-  },
-  {
-    name: 'Hot',
-    images: Images.coffe3,
-  },
-  {
-    name: 'Camomile',
-    images: Images.coffe1,
-  },
-];
-
-export const LIST_WINE = [
-  {
-    id: 1,
-    name: 'Reds Wine',
-    icons: 'wine-sharp',
-    actionName: 'reds',
-  },
-  {
-    id: 2,
-    name: 'Whites',
-    icons: 'ios-color-filter',
-    actionName: 'whites',
-  },
-  {
-    id: 3,
-    name: 'Sparkling',
-    icons: 'partly-sunny-outline',
-    actionName: 'sparkling',
-  },
-  {
-    id: 4,
-    name: 'Rose',
-    icons: 'md-rose-outline',
-    actionName: 'rose',
-  },
-  {
-    id: 5,
-    name: 'Dessert',
-    icons: 'fast-food-sharp',
-    actionName: 'dessert',
-  },
-  {
-    id: 6,
-    name: 'Reds Wine',
-    icons: 'wine-sharp',
-    actionName: 'reds',
-  },
-  {
-    id: 7,
-    name: 'Whites',
-    icons: 'ios-color-filter',
-    actionName: 'whites',
-  },
-  {
-    id: 8,
-    name: 'Sparkling',
-    icons: 'partly-sunny-outline',
-    actionName: 'sparkling',
-  },
-  {
-    id: 9,
-    name: 'Rose',
-    icons: 'md-rose-outline',
-    actionName: 'rose',
-  },
-  {
-    id: 10,
-    name: 'Dessert',
-    icons: 'fast-food-sharp',
-    actionName: 'dessert',
-  },
-];
+export const API_KEY = '48ee13341fed5652a9cb3a1ad019c06f';
 
 export const LANG = {
   eng: {
